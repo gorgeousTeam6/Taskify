b09c6af3890ded14b7e5f550d5be28acf75fa499
{dyk0224} <epn0224@gmail.com>
2024-08-10 00:44:19 +0900

    feat: 수정사항 반영, 버튼 분리, 메뉴 기능구현

---
+++ components/ButtonForDashboard/ButtonChildren/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './index.module.scss';
+import { IconCrown, IconArrowForward } from '@/assets/icongroup';
+
+export default function ButtonChildren({
+  buttonType,
+  color,
+  isOwner = false,
+  children,
+}: ButtonProps) {
+  const iconSize = 18;
+
+  return (
+  );
+}
